6d73bdc18c2b73de15fdbe8d20af95a7ec608980
Yulia Pianova <91152939+YuliaPianova@users.noreply.github.com>
2022-03-15 12:22:56 -0700

    Add files via upload

---
+++ Prop-it_up_assignment/README.md
@@ -0,0 +1,13 @@
+
+
+// Practicing React
+
+//Create a component called PersonCard that accepts the following props: 
+
+firstName
+lastName
+age
+hairColor
+
+Then, display four PersonCard components that show four different people, using props.
+
---
+++ Prop-it_up_assignment/src/components/PersonCard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Personcard = (props) => {
+    return(
+        <div>
+            <h2>{props.lastName}, {props.firstName}</h2>
+            
+            <h4> Age: {props.age}</h4>
+            <h4> Hair Color: {props.hairColor}</h4>
+        </div>
+    )
+}
+
+export default Personcard;
